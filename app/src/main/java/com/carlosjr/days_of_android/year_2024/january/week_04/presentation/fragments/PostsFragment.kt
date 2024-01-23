244734ebe686cd7f2f54850b335ea95d549d3cfe
carlos.junior <carlos.udi@live.com>
2024-01-22 21:14:06 -0300

    [Year_2024][Week_03] Added new button in InterfaceComponentsOneFragment
    Add image_view and contentDescription for accessibility in fragment_interface_components_one.

--- app/src/main/java/com/carlosjr/days_of_android/year_2024/january/week_04/presentation/fragments/PostsFragment.kt
+++ app/src/main/java/com/carlosjr/days_of_android/year_2024/january/week_04/presentation/fragments/PostsFragment.kt
@@ -1,4 +1,4 @@
-package com.carlosjr.days_of_android.year_2024.january.week_03.presentation.fragments
+package com.carlosjr.days_of_android.year_2024.january.week_04.presentation.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
